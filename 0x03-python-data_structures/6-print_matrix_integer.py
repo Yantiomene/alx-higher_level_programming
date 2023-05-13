927aeaf2b5375d288bf1abe74262b2c15379ffe9
Yaninthe Tiomene <yan2016tiomene@gmail.com>
2023-05-13 05:52:48 -0700

    [Fix] Fixing the case of an empty matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if not matrix:
-        print("")
+    if not matrix or len(matrix) == 1:
+        print()
     else:
         for row in matrix:
-            for i in row:
-                if row.index(i) != (len(row) - 1):
-                    print("{:d}".format(i), end=" ")
+            for i in range(len(row)):
+                if i < (len(row) - 1):
+                    print("{:d}".format(row[i]), end=" ")
                 else:
-                    print("{:d}".format(i))
+                    print("{:d}".format(row[i]))
