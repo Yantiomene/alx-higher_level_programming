2ada6aac54ef5ffc3a0bf3e53ed3f21b6afd6025
Yaninthe Tiomene <yan2016tiomene@gmail.com>
2023-05-13 06:10:22 -0700

    [Fix] Fixing a bug

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if not matrix or len(matrix) == 0 or len(matrix) == 1:
+    if not matrix or (len(matrix) == 1 and len(matrix[0]) == 0):
         print()
     else:
         for row in matrix:
