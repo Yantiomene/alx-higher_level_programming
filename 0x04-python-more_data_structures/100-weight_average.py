d00116d0d6183fe9e5e9861e0c8723d409399268
Yaninthe Tiomene <yan2016tiomene@gmail.com>
2023-05-17 11:05:37 -0700

    [Fix] Fixing a requirement

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
     '''Returns the weighted average of all integers tuple'''
-    if not my_list or len(my_list) == 0:
+    if not my_list:
         return (0)
-    w_avg = sum([x[0] *  x[1] for x in my_list]) / sum([x[1] for x in my_list])
+    w_avg = sum([x[0] * x[1] for x in my_list]) / sum([x[1] for x in my_list])
     return (w_avg)
