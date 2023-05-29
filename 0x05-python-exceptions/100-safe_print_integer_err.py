a1652450ca61988fb5c44daa698e7efcd15c72a3
Yaninthe Tiomene <yan2016tiomene@gmail.com>
2023-05-29 08:52:31 -0700

    [Feature] Adding a function to print an integer

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+    ''' prints an integer'''
+    import sys
+
+    try:
+        print("{:d}".format(value))
+    except (ValueError, TypeError) as err:
+        sys.stderr.write("Exception: {}\n".format(err))
+        return (False)
+    else:
+        return (True)
