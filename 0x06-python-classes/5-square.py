df639bc95d5d0eb8bf25fccb922ff5e2f5be5469
Yaninthe Tiomene <yan2016tiomene@gmail.com>
2023-05-30 13:53:47 -0700

    [Feature] Adding a my_print method

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+"""Define a Square class"""
+
+
+class Square:
+    """Defines a square with size private attibute
+    a size property, a property setter and an area
+    method"""
+    def __init__(self, size=0):
+        """Initialize the size"""
+        self.size = size
+
+    @property
+    def size(self):
+        """Method to retrieve the size"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """Set the value of the size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Calculate the area of the square"""
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """Prints the square with the character #"""
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                print("#" * self.__size)
