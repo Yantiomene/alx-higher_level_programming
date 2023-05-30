419eeac196befb087c18d4ac685eb002311976e0
Yaninthe Tiomene <yan2016tiomene@gmail.com>
2023-05-30 16:49:04 -0700

    [Feature] Adding a Magic class that define a circle

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import math
+
+"""Magic class"""
+
+
+class MagicClass:
+    """ Magic class that create a circle and evalute
+    it area and circumference"""
+    def __init__(self, radius=0):
+        """initialize the radius"""
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError("radius must be a number")
+        self.__radius = radius
+
+    def area(self):
+        """evaluate the area of the circle"""
+        return (math.pi * (self.__radius ** 2))
+
+    def circumference(self):
+        """evaluate the circumference of the circle"""
+        return (2 * math.pi * self.radius)
