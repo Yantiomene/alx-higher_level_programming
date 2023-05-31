723f350a415c3cf01ad78dc06c520273c79d20b9
Yaninthe Tiomene <yan2016tiomene@gmail.com>
2023-05-30 17:11:13 -0700

    [Fix] Fixing the bug in circumference method

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -6,7 +6,7 @@
 class MagicClass:
     """ Magic class that create a circle and evalute
     it area and circumference"""
-    def __init__(self, radius):
+    def __init__(self, radius=0):
         """initialize the radius"""
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
@@ -16,8 +16,8 @@ class MagicClass:
 
     def area(self):
         """evaluate the area of the circle"""
-        return (math.pi * (self.__radius ** 2))
+        return ((self.__radius ** 2) * math.pi)
 
     def circumference(self):
         """evaluate the circumference of the circle"""
-        return (2 * math.pi * self.radius)
+        return (2 * math.pi * self.__radius)
