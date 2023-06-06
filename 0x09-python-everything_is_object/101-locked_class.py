dd7cb40c6c23002380a7871e6f0cfe8cfabb0fcb
Yaninthe Tiomene <yan2016tiomene@gmail.com>
2023-06-06 15:00:43 -0700

    [Feature] Adding locked class to prevent a user to dynamically creating attribute

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines a class with no attribue or no class
+   that prevents the user from dynamically creating
+   new instance
+"""
+
+
+class LockedClass:
+    """LockedClass prevents user for dynamically
+       creating new instance
+    """
+    __slots__ = ['first_name']
+
+    def __init__(self):
+        """Init method"""
+        pass
