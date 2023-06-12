e9c9656c8f165bfe493546d2a1f71f07fd1b2593
Yaninthe Tiomene <yan2016tiomene@gmail.com>
2023-06-12 07:10:47 -0700

    [Feature] Adding a area() method that raises an exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Defines a BaseGeometry class"""
+
+
+class BaseGeometry:
+    """BaseGeometry class with a public class"""
+
+    def area(self):
+        """That raises an exception"""
+        raise Exception("area() is not implemented")
