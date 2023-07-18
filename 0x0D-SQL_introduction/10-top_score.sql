2fe8ebb7582baff6edc46d55ab63c0b03d492108
Yaninthe Tiomene <yan2016tiomene@gmail.com>
2023-07-18 12:19:44 -0700

    [Feature] Adding a script to list all record of a table order by a specific column

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- Lists all records of the table second_table
+-- score and name columns listed in this order and sorted
+-- score top first
+SELECT score, name FROM second_table ORDER BY score DESC;
