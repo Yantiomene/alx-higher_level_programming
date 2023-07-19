fdabcc53b194285207536c4b86f0fedacd2f5c6e
Yaninthe Tiomene <yan2016tiomene@gmail.com>
2023-07-19 08:11:22 -0700

    [feature] Adding a script to create a user and grants all privileges

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,9 @@
+-- Creates the MySQL server user_0d_1
+-- With all privileges on the server
+-- password set to user_0d_1_pwd
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+
+GRANT ALL PRIVILEGES
+ON *.*
+TO 'user_0d_1'@'localhost';
