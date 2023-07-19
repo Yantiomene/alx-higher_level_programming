9a8b61f7860445e6db74e35c8943a83eee538385
Yaninthe Tiomene <yan2016tiomene@gmail.com>
2023-07-19 10:46:32 -0700

    [Feature] Adding a script to lists all cities with the state name

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,6 @@
+-- Lists all cities by cities.id, cities.name, states.name
+-- sorted by cities.id
+SELECT c.id, c.name, s.name
+FROM cities AS c, states AS s
+WHERE c.state_id = s.id
+ORDER BY c.id;
