5945392aa5d82e242e7188d483b7db41da16746a
Yaninthe Tiomene <yan2016tiomene@gmail.com>
2023-07-19 14:40:44 -0700

    [Feature] Adding a script to list all shows and genres likned to that show

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,9 @@
+-- Lists all shows and genres
+SELECT s.title, g.name
+FROM tv_shows AS s
+     LEFT JOIN tv_show_genres AS sg
+     ON sg.show_id = s.id
+     LEFT JOIN tv_genres AS g
+     ON g.id = sg.genre_id
+GROUP BY s.title, g.name
+ORDER BY s.title, g.name;
