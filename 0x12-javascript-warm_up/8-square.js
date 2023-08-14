f6c6b3e49d2484caaac5d7deb67e3b51f54d67d9
Yaninthe Tiomene <yan2016tiomene@gmail.com>
2023-08-14 08:38:26 -0700

    [Feature] Adding a script to print a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,7 +2,7 @@
 
 const size = parseInt(process.argv[2]);
 let square;
-if (Number.isNan(size)) {
+if (Number.isNaN(size)) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < size; i++) {
