c132d4d9b8f7422a6ce40fe166683d9084d353d1
Yaninthe Tiomene <yan2016tiomene@gmail.com>
2023-08-15 07:31:33 -0700

    [Fix] Fixing  a character output

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -10,7 +10,7 @@ class Square extends SquareS {
       for (let i = 0; i < this.height; i++) {
         let rect = '';
         for (let j = 0; j < this.width; j++) {
-          rect += 'C';
+          rect += c;
         }
         console.log(rect);
       }
