e36fade664a67cb13343502aabf0c977c48ddc2d
Yaninthe Tiomene <yan2016tiomene@gmail.com>
2023-08-15 07:04:57 -0700

    [Fix] Fixing the output format

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -9,10 +9,8 @@ class Rectangle {
   }
 
   print () {
-    let rect;
-
     for (let i = 0; i < this.height; i++) {
-      rect = ' ';
+      let rect = '';
       for (let j = 0; j < this.width; j++) {
         rect += 'X';
       }
