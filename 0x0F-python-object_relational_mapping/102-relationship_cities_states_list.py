7f0ade8e3a5f00df8f9e90454934d42c07ec74f2
Yaninthe Tiomene <yan2016tiomene@gmail.com>
2023-08-18 11:53:14 -0700

    [Feature] Adding a script to retrieve citires and stated base on relationship

---
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+script to fetch all states from the database
+"""
+
+from sqlalchemy import (create_engine)
+from relationship_state import State, Base
+from relationship_city import City
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    cities = session.query(City).order_by(City.id).all()
+    for city in cities:
+        print("{}: {} -> {}".format(city.id, city.name, city.state.name))
