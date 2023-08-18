75148ba0f276c18a4034ef302dfb04ad22e8e160
Yaninthe Tiomene <yan2016tiomene@gmail.com>
2023-08-18 09:48:11 -0700

    [Feature] Adding a script to retrieve all cities in states

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+script to fetch all states from the database
+"""
+
+from sqlalchemy import (create_engine)
+from model_state import State, Base
+from sqlalchemy.orm import sessionmaker
+import sys
+from model_city import City
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    stat_city = session.query(State, City).order_by(State.id).\
+        filter(State.id == City.state_id).all()
+    for state, city in stat_city:
+        print("{} : ({}) {}".format(state.name, city.id, city.name))
