6a1bb24c13e2aff841aff43dac34e4f57422c201
Yaninthe Tiomene <yan2016tiomene@gmail.com>
2023-08-18 08:36:55 -0700

    [Feature] Adding a script to fecth all states containing the letter a

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+script to fetch all states from the database
+"""
+
+from sqlalchemy import (create_engine)
+from model_state import State, Base
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).order_by(State.id).filter(State.name.
+                                                           like('%a%')).all()
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
