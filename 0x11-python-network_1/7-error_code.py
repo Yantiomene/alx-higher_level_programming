b0b1c4f175db749a343cc0d67048efc2b484e3e3
Yaninthe Tiomene <yan2016tiomene@gmail.com>
2023-09-01 13:33:45 -0700

    [Fix] Fixing a bug in the request

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,8 +6,8 @@ import sys
 
 if __name__ == '__main__':
     try:
-        r = requests(sys.argv[1])
-        if (r.status_code >= 400):
+        r = requests.get(sys.argv[1])
+        if r.status_code >= 400:
             print("Error code: {}".format(r.status_code))
         else:
             print(r.text)
